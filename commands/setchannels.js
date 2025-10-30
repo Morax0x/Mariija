9f94cc0eb52d024fd35ff1096a5a28e9623c8a00
Morax0x <aziz6kd@gmail.com>
2025-10-31 01:11:38 +0400

    Update setchannels.js

--- commands/setchannels.js
+++ commands/setchannels.js
@@ -1,26 +1,14 @@
-// 📁 commands/setchannels.js
-// (هذا الكود الصح حق مراقبة القنوات)
+// 📁 commands/setchannels.js (النسخة 8.0 - تدعم السيرفرات)
 
-import {
-    processChannels // ⬅️ هذا يستدعي الدالة اللي تقبل الكاتاغوري والفورم
-} from '../utils.js';
+import { processChannels } from '../utils.js';
 
 export default {
     name: 'setchannels',
-    description: 'إضافة قنوات نصية أو ثريدات/بوستات للمراقبة.',
-    adminOnly: true, // ⬅️ سيتم التحقق من الصلاحيات تلقائياً
+    description: '[إدارة] إضافة قنوات نصية أو ثريدات/بوستات للمراقبة (يدعم الكاتاغوري).',
+    adminOnly: true,
 
-    /**
-     * @param {Client} client
-     * @param {import("discord.js").Message | import("discord.js").ChatInputCommandInteraction} interactionOrMessage
-     * @param {String[]} args (للأوامر التقليدية) | {InteractionOptions} args (للسلاش)
-     * @param {Database} db
-     */
     async execute(client, interactionOrMessage, args, db) {
-
-        // ببساطة، نستدعي الدالة ونعطيها وضع 'add'
-        // الدالة نفسها (processChannels) ستقوم بكل العمل
-        await processChannels(interactionOrMessage, args, 'add', db, client);
-
+        // (الدالة المساعدة محدثة وتستخدم guildId من interactionOrMessage)
+        return processChannels(interactionOrMessage, args, 'add', db, client);
     }
 };
